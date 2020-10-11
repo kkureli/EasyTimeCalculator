f1ca5a1c2e15e828284492f8aedaa8ed82e532a0
Kaan <kkureli@gmail.com>
2020-10-11 18:25:01 +0300

    Updates

---
+++ precache-manifest.da3fdbf7db46134f11ab34b53f77318e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c1d02bce3d9cc7586e451c726e250b9",
+    "url": "/EasyTimeCalculator/index.html"
+  },
+  {
+    "revision": "5c36dfd2d928d99ee36a",
+    "url": "/EasyTimeCalculator/static/css/main.d3e993ac.chunk.css"
+  },
+  {
+    "revision": "9cdcc1f3337e68df6cfb",
+    "url": "/EasyTimeCalculator/static/js/2.fb473574.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EasyTimeCalculator/static/js/2.fb473574.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c36dfd2d928d99ee36a",
+    "url": "/EasyTimeCalculator/static/js/main.71dfc3ad.chunk.js"
+  },
+  {
+    "revision": "40c7c3f48897e1e39f8e",
+    "url": "/EasyTimeCalculator/static/js/runtime-main.2f6894f8.js"
+  },
+  {
+    "revision": "24967bfd29c770f098770b8a78e3e9c6",
+    "url": "/EasyTimeCalculator/static/media/app.24967bfd.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EasyTimeCalculator/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dff0b66cc51636204ab3074df6beab98",
+    "url": "/EasyTimeCalculator/static/media/logoTimeCal.dff0b66c.png"
+  }
+]);
